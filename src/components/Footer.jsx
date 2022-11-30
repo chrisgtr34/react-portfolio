b4dcfed6b0500ee75b759db17c018eae576f5315
Christopher <chris@CHRISs-MBP.attlocal.net>
2022-11-29 17:01:45 -0800

    debugging gh pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,7 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer>
-      <a href="#home" className="footer__logo">Christopher Gomez</a>
-      <ul className="permalinks">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Blog</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+
       <div className="footer__socials">
       <a href="https://www.linkedin.com/in/christopher-gomez-240229139/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
       <a href="https://github.com/chrisgtr34" target="_blank" rel="noreferrer" ><FaGithub /></a>
