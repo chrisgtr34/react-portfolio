9c8c251376a92c3cf62781f98647be7e24410f08
Christopher <chris@CHRISs-MBP.attlocal.net>
2022-12-07 16:23:17 -0800

    debugging img locations and code to display

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import weather from '../../images/weather.png'
 
 export default function Projects() {
   return (
     <div>
-      <h1 
-
-      style={{color:'white', margin:30}}>Projects
-
+      <h1
+        style={{ color: 'white', margin: 30 }}>Projects
       </h1>
-        
-      <h3 style={{color:'white', margin:30}}>
-      Here are a few projects showcasing some of the work ive done thus far.
-      More projects soon to follow as I advance my skills. 
+
+      <h3 style={{ color: 'white', margin: 30 }}>
+        Here are a few projects showcasing some of the work ive done thus far.
+        More projects soon to follow as I advance my skills.
       </h3>
-    </div>
+
+      <div>
+        <a href="https://chrisgtr34.github.io/weather-dashboard/" target="_blank" rel="noreferrer" class name="weather">
+          <img  src={weather} className="weather" alt="weather.png" />
+        </a>
+      </div>
+      <div>
+        <a href="https://codingbeautydev.com" target="_blank" rel="noreferrer" class name="weather">
+          <img  src={weather} alt="weather.png" />
+        </a>
+        <div>
+        <a href="https://codingbeautydev.com" target="_blank" rel="noreferrer" class name="weather">
+          <img  src={weather} alt="weather.png" />
+        </a>
+      </div>
+      </div>
+    </div >
   );
 }
