2ce4c86cc217fc1277e16679c9112ecf4eff0328
Christopher <chris@CHRISs-MBP.attlocal.net>
2022-12-08 16:18:19 -0800

    adding into contact and proejct section

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import weather from '../../images/weather.png'
+import ecommerce from '../../images/ecommerce.png'
+
 
 export default function Projects() {
   return (
@@ -12,21 +14,24 @@ export default function Projects() {
         Here are a few projects showcasing some of the work ive done thus far.
         More projects soon to follow as I advance my skills.
       </h3>
-
+      <br></br>
+      <br></br>
       <div>
-        <a href="https://chrisgtr34.github.io/weather-dashboard/" target="_blank" rel="noreferrer" class name="weather">
-          <img  src={weather} className="weather" alt="weather.png" />
+        <a href="https://chrisgtr34.github.io/weather-dashboard/" target="_blank" rel="noreferrer" >
+          <img src={weather} className="weather" alt="weather.png" />
+          <h2>
+            Weather Dashboard
+          </h2>
         </a>
       </div>
+      
       <div>
-        <a href="https://codingbeautydev.com" target="_blank" rel="noreferrer" class name="weather">
-          <img  src={weather} alt="weather.png" />
+        <a href="https://drive.google.com/file/d/1F8EVs6XG1tDzjmWHRKr5VEDzIT7YKxS-/view" target="_blank" rel="noreferrer" class name="ecommerce">
+          <img src={ecommerce} className="ecommerce" alt="ecommerce.png" />
+          <h2>
+            Ecommerce 
+          </h2>
         </a>
-        <div>
-        <a href="https://codingbeautydev.com" target="_blank" rel="noreferrer" class name="weather">
-          <img  src={weather} alt="weather.png" />
-        </a>
-      </div>
       </div>
     </div >
   );
